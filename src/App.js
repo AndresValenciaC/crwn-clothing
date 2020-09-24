8646138ce12822779e43a84e2f2874a876554299
Andres_Valencia <andres228cv@gmail.com>
2020-09-24 12:17:21 -0500

    Implemnented firebase Utils, inclede ability to store authenticated users into fireStore Database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 import Header from "./components/header/header.component.jsx";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor(props) {
@@ -19,9 +19,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            id: snapshot.id,
+            ...snapshot.data(),
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
+    console.log(this.state);
   }
 
   componentWillUnmount() {
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./sign-in-and-sign-up.styles.scss";
 import SignIn from "../../components/signIn/sign-in-component";
+import SignUp from "../../components/sign-up/sign-up.component";
 const SignInAndSignOutPage = () => (
   <div className="sign-in-sign-out">
     <SignIn />
+    <SignUp />
   </div>
 );
 
